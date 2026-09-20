c1b41e6003ddddd28b009b64637849840439bb9b
Sammanita06 <sammanitasingh.0612@gmail.com>
2026-09-20 17:39:45 +0530

    Fix formatting and add newline at end of file

--- custom_transformers.py
+++ custom_transformers.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin, OneToOneFeatureMixin
 
+
 class MaintenanceFeatureEngineer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
